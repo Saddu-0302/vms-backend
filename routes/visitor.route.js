e42b31cf440194416127688b992035466edecf4a
mohammadsad <saadsayyad0302@gmail.com>
2025-07-06 13:36:57 +0530

    added update status route

--- routes/visitor.route.js
+++ routes/visitor.route.js
@@ -5,5 +5,6 @@ const visitorController = require("../controllers/visitor.controller")
 router
     .get("/", visitorController.getAllVisitors)
     .post("/add-visitor", visitorController.addVisitor)
+    .put("/status/:id", visitorController.updateStatus)
 
 module.exports = router
